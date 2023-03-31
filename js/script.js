a18015d592322d0333328d293321b98ab5e3ba37
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-03-31 16:55:04 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a Trapezoid
+ */
+function calculate () {
+  // input
+  const baseA = parseInt(document.getElementById('base-a').value)
+  const baseB = parseInt(document.getElementById('base-b').value)
+  const height = parseInt(document.getElementById('height').value)
+
+
+
+  // process
+  const area = [(baseA + baseB) / 2] * height
+
+  // output
+  document.getElementById('area').innerHTML = 'area is: ' + area + ' cm²'
+}
